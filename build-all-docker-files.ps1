416927f0aa7db6a6821a00dbe9ebb3d3d257beff
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-09-30 15:38:09 +0700

    first push

---
+++ build-all-docker-files.ps1
@@ -0,0 +1,3 @@
+docker build -f services/interview-system-config-server/Dockerfile -t hoofkhanh/interview-system-config-server:1.0.0 .
+docker build -f services/interview-system-discovery-server/Dockerfile -t hoofkhanh/interview-system-discovery-server:1.0.0 .
+docker build -f services/interview-system-user-service/Dockerfile -t hoofkhanh/interview-system-user-service:1.0.0 .
---
+++ services/interview-system-user-service/src/main/java/com/hokhanh/InterviewSystemUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hokhanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSystemUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSystemUserServiceApplication.class, args);
+	}
+
+}
