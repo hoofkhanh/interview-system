e667072b4b5f72058227947e67aa43d3330a0d72
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-09-30 15:42:33 +0700

    first push

--- services/interview-system-discovery-server/src/test/java/com/hokhanh/InterviewSystemDiscoveryServerApplicationTests.java
+++ services/interview-system-discovery-server/src/test/java/com/hokhanh/InterviewSystemDiscoveryServerApplicationTests.java
@@ -1,13 +1,9 @@
 package com.hokhanh;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InterviewSystemDiscoveryServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
