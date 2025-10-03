f4aedb6c4acf43a9aff372bb1eae260359ace5ee
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-10-03 23:41:12 +0700

    set up gateway, auth services

--- services/interview-system-auth-service/src/test/java/com/hokhanh/InterviewSystemAuthServiceApplicationTests.java
+++ services/interview-system-auth-service/src/test/java/com/hokhanh/InterviewSystemAuthServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package com.hokhanh;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InterviewSystemAuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
