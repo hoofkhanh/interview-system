174ed2640cec8f09ca3464b2efd6a12ee8a587d0
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-10-09 15:39:31 +0700

    update docker compose to containers work in docker

--- commons/interview-system-common-libary/src/main/java/com/hokhanh/common/utils/EnvLoaderUtil.java
+++ commons/interview-system-common-libary/src/main/java/com/hokhanh/common/utils/EnvLoaderUtil.java
@@ -5,22 +5,31 @@ import io.github.cdimascio.dotenv.Dotenv;
 public final class EnvLoaderUtil {
 	private static final Dotenv DOT_ENV = Dotenv.configure().directory("../../").load();
 
+//	private static String get(String key) {
+//		return DOT_ENV.get(key);
+//	}
+
 	private static String get(String key) {
+		String value = System.getenv(key);
+		if (value != null) {
+			return value;
+		}
+
 		return DOT_ENV.get(key);
 	}
 
 	public static String getJwtSecretKey() {
 		return get("JWT_SECRET_KEY");
 	}
-	
+
 	public static Long getJwtAccessTokenExpiration() {
 		return Long.parseLong(get("JWT_ACCESS_TOKEN_EXPIRATION"));
 	}
-	
+
 	public static Long getJwtRefreshTokenExpiration() {
 		return Long.parseLong(get("JWT_REFRESH_TOKEN_EXPIRATION"));
 	}
-	
+
 	private EnvLoaderUtil() {
 	}
 }
